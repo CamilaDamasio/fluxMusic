fb952502d25ac4606e588532cd05480b0423fb7d
CamilaDamasio <camiladamasioef@hotmail.com>
2022-04-22 13:47:12 -0300

    Feat: render artists information to artists page

--- fluxMusic_front/src/pages/Artists.js
+++ fluxMusic_front/src/pages/Artists.js
@@ -1,15 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import '../styles/artists.css';
 import { api } from '../Api';
 
 
-export default function Home(props) {
+export default function Artists(props) {
+  const [artists, setArtists] = useState();
+  console.log('Artists: ', artists);
+
   useEffect(() => {
-    api.get('/artists').then(res => console.log(res));
+    api.get('/artists').then(res => setArtists(res.data.root.data[0].item));
   }, []);
 
   return (
-    <div>
+    <div className='home-page'>
       <h2>Estou na Artists!</h2>
+      { artists ? (
+        <div className='body-news'>
+          <div className='news'>
+        {artists.map((item) => 
+          <div className='new'>
+            <h5>{item.name}</h5>
+          </div>
+          )}
+          </div>
+        </div>
+      ) : null }
     </div>
   )
 }
